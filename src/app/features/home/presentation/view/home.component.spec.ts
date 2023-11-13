d304cb70039a13d97b5349581899c81e5c1e333f
Fernando Michetti <fernandomichetti@gmail.com>
2023-11-13 19:12:57 -0300

    Ajustando testes que estavam quebrados

--- src/app/features/home/presentation/view/home.component.spec.ts
+++ src/app/features/home/presentation/view/home.component.spec.ts
@@ -1,21 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { HomeViewModelService } from '../view-model/home-view-model.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let homeViewModelService: HomeViewModelService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [HomeComponent],
+      providers: [HomeViewModelService],
     });
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    homeViewModelService = TestBed.inject(HomeViewModelService);
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize the view model on ngOnInit', () => {
+    spyOn(homeViewModelService, 'initViewModel');
+    component.ngOnInit();
+    expect(homeViewModelService.initViewModel).toHaveBeenCalled();
+  });
+
+  it('should destroy the view model on ngOnDestroy', () => {
+    spyOn(homeViewModelService, 'destroyViewModel');
+    component.ngOnDestroy();
+    expect(homeViewModelService.destroyViewModel).toHaveBeenCalled();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 });
